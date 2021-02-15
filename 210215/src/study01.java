73274128e4a41150267534005a73dcd1ab087140
Roma_Man <62382377+kjspo56@users.noreply.github.com>
2021-02-15 18:16:25 +0900

    Add files via upload
    몸이 아프지만 그래도 포기하지 않고 1개라도 커밋하기...

---
+++ 210215/src/study01.java
@@ -0,0 +1,11 @@
+//10172번
+public class study01 {
+	public static void main(String[] args) {
+		
+		System.out.println("|\\_/|"); // \방지하기 위해 1개 더 사용
+		System.out.println("|q p|  /}");
+		System.out.println("( 0 )\"\"\"\\"); //따옴표 안뭉치게 하나씩 잡아넣어줬음.
+		System.out.println("\"^\"`  |");
+		System.out.println("||_/=\\\\__|"); // \ 방지하기 위해 1개 더 사용
+	}
+}
